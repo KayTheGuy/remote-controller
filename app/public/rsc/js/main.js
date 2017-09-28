096c7cdca71c5a5340c5778ff6e752ad1d279f6c
Kayhan <kdehghan@sfu.ca>
2017-09-27 20:26:54 -0700

    Finish music player for Mac OS. Remove redundant CSS lines and files. Change styling of the index.html

--- app/public/rsc/js/main.js
+++ app/public/rsc/js/main.js
@@ -1,27 +1,66 @@
 const homeUrl = window.location.href;
+var currentSongId = -1;
+var listSize = 0;
 
-var songPlay = function() {
-    $.get(homeUrl + 'play', function(data, status) {
-        console.log(status);
+var playSong = function(id) {
+    $.get(homeUrl + 'play/' + id, function(data, status) {
+        if(status === 'success') {
+            currentSongId = id;
+        }
+    });
+};
+
+var stopSong = function() {
+    $.get(homeUrl + 'stop', function(data, status) {
+    });
+};
+
+var pauseResume = function() {
+    $.get(homeUrl + 'pause', function(data, status) {
     });
 };
 
 // get songs list
 $.get(homeUrl + 'list', function(result){
     let jsonList = JSON.parse(result);
+    listSize = jsonList.length;
     var list = $(".list-control");
     // populate songs list
     jsonList.forEach(function(element) {
-        list.append(
-            '<li id=' + element.id
-            + 'class="list-song"><span class="mdl-chip mdl-chip--contact"><span class="mdl-chip__contact mdl-color--teal mdl-color-text--white">'
+        var song = $('<li id="' + element.id
+            + '" class="list-song"><span class="mdl-chip mdl-chip--contact"><span class="mdl-chip__contact mdl-color--teal mdl-color-text--white">'
             + '<i class="material-icons">audiotrack</i></span><span class="mdl-chip__text">'
             + element.name
             + '</span></span></li>'
-        ).click(songPlay);
+        );
+        list.append(song);
+        song.click(function() {
+            playSong(element.id);
+        });
     });
 }); 
 
 
-$('#playBtn').click(songPlay);
+$('#stopBtn').click(function() {
+    stopSong();
+});
+
+$('#pauseResumeBtn').click(function() {
+    pauseResume();
+});
+
+$('#prevBtn').click(function() {
+    let id = currentSongId - 1;
+    if(id >= 0 && id < listSize) {
+        playSong(id);
+    }
+});
+
+$('#nextBtn').click(function() {
+    let id = currentSongId + 1;
+    console.log(id);
+    if(id >= 0 && id < listSize) {
+        playSong(id);
+    }
+});
 
--- app/server.js
+++ app/server.js
@@ -2,20 +2,53 @@
 const fs = require('fs');
 const path = require('path');
 const http = require('http');
-const spawn = require('child_process').spawn;
+const ostype = require('os').type();
+const child_process = require('child_process');
 
+var playProcess = 0;
+var isPaused = false;
+/**
+ * Windows Music Player Configs
+ * ...
+ */
+var playerWindows = function(songId, command) {
 
+}; 
 
 /**
- * Windows Configs
+ * Mac Music Player Configs
  * ...
-*/
+ */
+var playerMac = function(songId, command) {
+    if(command === 'PLAY') {
+        // if aother music is plsying kill process start a new one
+        if(!!playProcess) {
+            stopProcess = child_process.execSync('kill -9 ' + playProcess.pid);
+            playProcess.kill(); // kill old process
+        }
+        if(!!songsList[songId].path) { // check if path to song exists
+            playProcess = child_process.spawn('afplay', ['-q', '1',songsList[songId].path]);
+        }
+    } else if (command === 'STOP') {
+        if(!!playProcess) {
+            playProcess = child_process.execSync('killall afplay');
+            playProcess = 0;
+        }
+    } else if (command === 'PAUSE') {
+        if(!!playProcess) {
+            if(!isPaused) { // pause the music
+                puaseProcess = child_process.execSync('kill -17 ' + playProcess.pid);
+                isPaused = true;
+            } else {
+                resumeProcess = child_process.execSync('kill -19 ' + playProcess.pid);
+                isPaused = false;
+            }
+        }
+    }
+};
 
-
-/**
- * Mac Configs
- * ...
-*/
+// music player OS type pair
+const osPlayerPair = {'Darwin': playerMac, 'Windows_NT': playerWindows};
 
 // chech for music folder directory
 if(process.argv.length < 3) {
@@ -30,7 +63,7 @@ const musicDir = process.argv[2];
  *  create array of songs (json)  (synchronized)
  *  currently supports: .mp3, .aif, .wav
  */
-var songsList;
+var songsList = [];
 var i = 0;
 var walkSongsSync = function(dir, songsList) {
     let fileNames = fs.readdirSync(dir);
@@ -48,8 +81,6 @@ var walkSongsSync = function(dir, songsList) {
     return songsList;
 }
 
-
-
 if(fs.existsSync(musicDir)) {
     songsList = walkSongsSync(musicDir);
 } else {
@@ -62,50 +93,55 @@ songsList.forEach(function(element) {
     songsListClient.push({'id': element.id,'name': element.name});
 });
 
-
 var server = http.createServer(function(request, response) {
-    
-    if(request.url === '/') {
-        fs.readFile('./public/index.html', function(err, html) {
-            if(!err) {
-                response.writeHead(200, {'Content-Type': 'text/html'});
-                response.end(html);
-            } else {
-                response.writeHead(200, {'Content-Type': 'text/plain'});
-                response.end('500 Internal Server Error: Unable to read the files.');
-            }
-        });
-    } else if (request.url.match(/.css$/)) {
-        response.writeHead(200, {'Content-Type': 'text/css'});
-        var cssPath = path.join(__dirname, 'public', request.url);
-        fs.createReadStream(cssPath, 'UTF-8').pipe(response);
-    } else if (request.url.match(/.js$/)) {
-        response.writeHead(200, {'Content-Type': 'text/js'});
-        var jsPath = path.join(__dirname, 'public', request.url);
-        fs.createReadStream(jsPath).pipe(response);
-    } else if (request.url.match(/.jpeg$/)) {
-        response.writeHead(200, {'Content-Type': 'img/jpeg'});
-        var imgPath = path.join(__dirname, 'public', request.url);
-        fs.createReadStream(imgPath).pipe(response);
-    } else if (request.url === '/play') {
-        var playerProcess = spawn('afplay', ['-q', '1','song/1.mp3']);
-        
-        setTimeout(function() {
-            var playerProcess = spawn('killall', ['afplay']);
-        }, 8000);
-        
-        response.writeHead(200, {'Content-Type': 'text/plain'});
-        response.end('yo2');
-    } else if (request.url === '/list') {
-        response.writeHead(200, {'Content-Type': 'text/js'});
-        response.end(JSON.stringify(songsListClient));
+    if(request.method === 'GET') {
+        if(request.url === '/') {
+            fs.readFile('./public/index.html', function(err, html) {
+                if(!err) {
+                    response.writeHead(200, {'Content-Type': 'text/html'});
+                    response.end(html);
+                } else {
+                    response.writeHead(200, {'Content-Type': 'text/plain'});
+                    response.end('500 Internal Server Error: Unable to read the files.');
+                }
+            });
+        } else if (request.url.match(/.css$/)) {
+            response.writeHead(200, {'Content-Type': 'text/css'});
+            var cssPath = path.join(__dirname, 'public', request.url);
+            fs.createReadStream(cssPath, 'UTF-8').pipe(response);
+        } else if (request.url.match(/.js$/)) {
+            response.writeHead(200, {'Content-Type': 'text/js'});
+            var jsPath = path.join(__dirname, 'public', request.url);
+            fs.createReadStream(jsPath).pipe(response);
+        } else if (request.url.match(/.jpeg$/)) {
+            response.writeHead(200, {'Content-Type': 'img/jpeg'});
+            var imgPath = path.join(__dirname, 'public', request.url);
+            fs.createReadStream(imgPath).pipe(response);
+        } else if (request.url.match(/^\/play\/(\d)+$/g)) {
+            let player = osPlayerPair[ostype];
+            let songId = request.url.slice(6);
+            player(songId, 'PLAY');
+            response.writeHead(200, {'Content-Type': 'text/plain'});
+            response.end();
+        } else if (request.url === '/stop') {
+            let player = osPlayerPair[ostype];
+            player(null, 'STOP');
+            response.writeHead(200, {'Content-Type': 'text/plain'});
+            response.end();
+        } else if (request.url === '/pause') {
+            let player = osPlayerPair[ostype];
+            console.log(player);
+            player(null, 'PAUSE');
+            response.writeHead(200, {'Content-Type': 'text/plain'});
+            response.end();
+        } else if (request.url === '/list') {
+            response.writeHead(200, {'Content-Type': 'text/js'});
+            response.end(JSON.stringify(songsListClient));
+        }
     } else {
+        console.log(request.url);
         response.writeHead(404, {'Content-Type': 'text/plain'});
         response.end('Error 404: Not Found');
     }
     
 }).listen('8000');
-
-
-// TODO: create array of json songs and send it to client
-// TODO: create list on client side
